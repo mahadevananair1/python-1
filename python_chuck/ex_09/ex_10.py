c28ea7f277d22c70cc5bd487354359f74aeaad3d
JanPoonthong <janpoonthong@gmail.com>
2020-07-06 12:14:41 +0700

    Create ex_10.py

---
+++ python_chuck/ex_09/ex_10.py
@@ -0,0 +1,27 @@
+fname = input('Enter file name: ')
+if len(fname) < 1:
+    fname = 'clown.txt'
+fhand = open(fname)
+
+di = dict()
+for line in fhand:
+    line = line.rstrip()
+    wds = line.split()
+    for w in wds:
+        # idiom: retrieve/create/update counter
+        di[w] = di.get(w, 0) + 1
+
+#print(di)
+
+tmp = list()
+for k, v in di.items():
+    newtup = (v, k)
+    tmp.append(newtup)
+
+#print('Flipped', tmp)
+
+tmp = sorted(tmp, reverse=True)
+#print('Sorted', tmp[:5])
+
+for v, k in tmp[:5]:
+    print(k, v)
