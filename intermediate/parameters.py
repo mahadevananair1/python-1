5c6ee39056186e75c24ce8dc0f1d940ab3d1a47e
JanPoonthong <janpoonthong@gmail.com>
2020-07-11 10:21:17 +0700

    add

--- intermediate/parameters.py
+++ intermediate/parameters.py
@@ -1,4 +1,16 @@
-def func(word, freq):
-    print(word * freq)
+class car(object):
+  def __init__(self, make, model, year, condition, kms):
+    self.make = make
+    self.model = model
+    self.year = year
+    self.condition = condition
+    self.kms = kms
 
-call = func('Jan', 5)
+  def display(self, showAll):
+    if showAll:
+      print('This car is a %s %s from %s, it is %s and has %s kms.' %(self.make, self.model, self.year, self.condition, self.kms))
+    else:
+      print('This car is a %s %s from %s.' %(self.make, self.model, self.year))
+
+whip = car('Ford', 'Fusion', 2012, 'New', 0)
+whip.display(True)
