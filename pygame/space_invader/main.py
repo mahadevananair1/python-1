4667aa5b53bcd7597aca48dec6731d4618d74f08
jan <janpoonthong628@gmail.com>
2020-07-12 07:43:16 +0700

    pygame

---
+++ pygame/space_invader/main.py
@@ -0,0 +1,45 @@
+import pygame
+
+pygame.init()
+
+# Create the screen
+screen = pygame.display.set_mode((800, 600))
+
+# Caption and Icon
+pygame.display.set_caption('Pygame')
+icon = pygame.image.load('vr-gaming.png')
+pygame.display.set_icon(icon)
+
+# Player
+player_img = pygame.image.load('space-invaders.png')
+player_x = 370
+player_y = 480
+player_x_change = 0
+
+def player(x, y):
+    screen.blit(player_img, (x, y))
+# Game loop
+running = True
+while running:
+    screen.fill((153, 209, 255))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        # If keystroke is pressed check whether it's right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player_x_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                player_x_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                player_x_change = 0
+
+    player_x += player_x_change
+    if player_x <= 0:
+        player_x = 0
+    if player_x >= 736:
+        player_x = 736
+    player(player_x, player_y)
+    pygame.display.update()
