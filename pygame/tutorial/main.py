bef962f8d0b9efb4f335786b42699f2a7744d0d5
jan <janpoonthong628@gmail.com>
2020-07-12 07:29:58 +0700

    pygame

--- pygame/tutorial/main.py
+++ pygame/tutorial/main.py
@@ -1,6 +1,5 @@
 import pygame
 
-# Intialize the pygame
 pygame.init()
 
 # Create the screen
@@ -15,13 +14,28 @@ pygame.display.set_icon(icon)
 player_img = pygame.image.load('space-invaders.png')
 player_x = 370
 player_y = 480
+player_x_change = 0
 
+def player(x, y):
+    screen.blit(player_img, (x, y))
 # Game loop
 running = True
 while running:
+    screen.fill((153, 209, 255))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # If keystroke is pressed check whether it's right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player_x_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                player_x_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                player_x_change = 0
 
-    screen.fill((153, 209, 255))
+    player_x += player_x_change
+    player(player_x, player_y)
     pygame.display.update()
