6cc9b8a7d0776d6699096ec0e7f3d3d36b1b1e7f
Jan <janpoonthong628@gmail.com>
2020-07-18 15:37:09 +0700

    gui

--- projects/password_generate/gui_password_generator.py
+++ projects/password_generate/gui_password_generator.py
@@ -1,12 +1,42 @@
-from tkinter import  *
+from tkinter import *
+import pyperclip
+import random
 
 # Set screen
 root = Tk()
 root.geometry("400x400")
 root.title("Password Generator")
 
-draw_title = Label(root, text="PASSWORD GENERATOR", width="300", height="3", font=("Times", 20)).pack() 
-draw_password = Label(root, text="Password", font=("Helvetica", 16)).pack()
-button = Button(root, text="Generate").pack()
+# Value
+passstr = StringVar()
+passlen = IntVar()
 
+
+def generate():
+    pass1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+             'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+             'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+             'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+             'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+             'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8',
+             '9', '0', ' ', '!', '@', '#', '$', '%', '^', '&',
+             '*', '(', ')']
+    password = ""
+    for i in range(passlen.get()):
+        password += random.choice(pass1)
+    passstr.set(password)
+
+def copytoclipboard():
+    random_password = passstr.get()
+    pyperclip.copy(random_password)
+
+# Draw
+Label(root, text="PASSWORD GENERATOR", width="300", height="2", font=("Times", 20)).pack()
+
+Label(root, text="Enter Password Length", width="100", height="1", font=("Helvetica", 16)).pack()
+Entry(root, textvariable=passlen, width="30").pack()
+Button(root, text="Generate Password", command=generate).pack()
+
+Entry(root, textvariable=passstr, width="30").pack()
+Button(root, text="Copy to clipboard", command=copytoclipboard).pack()
 root.mainloop()
