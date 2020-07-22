c5bbe87c911bd70698bf537824d971150317a20a
jan <janpoonthong628@gmail.com>
2020-07-23 06:20:36 +0700

    gui

--- pygame/tic_tac_toe/gui/gui_tic_tac_toe.py
+++ pygame/tic_tac_toe/gui/gui_tic_tac_toe.py
@@ -8,8 +8,14 @@ width, height = 600, 500
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Tic Tac Toe")
 
+# loading images
 x_img = pygame.image.load("/home/jan/code/python/pygame/tic_tac_toe/gui/x.png")
 o_img = pygame.image.load("/home/jan/code/python/pygame/tic_tac_toe/gui/o.png")
+
+# resizing images
+width_resize, height_resize = 110, 110
+x_img = pygame.transform.scale(x_img, (width_resize, height_resize))
+o_img = pygame.transform.scale(o_img, (width_resize, height_resize))
 
 def draw_x():
     screen.blit(x_img, (25, 15))
@@ -17,14 +23,18 @@ def draw_x():
 def draw_o():
     screen.blit(o_img, (250, 200))
 
+def draw_XO():
+    if pos == cell_one:
+        screen.blit(x_img)
+
 def click_to_cell():
-    if pos:
-        print(pos)
     if pos[0] < 200 and pos[1] < 150:
         cell_one = pos
         print("Cell one")
     elif pos[0] >= 200 and pos[0] < 400 and pos[1] < 150:
         cell_two = pos
+        if pos == cell_two:
+            screen.blit(x_img, (100,200))
         print("Cell two")
     elif pos[0] >= 400 and pos[0] < 600 and pos[1] < 150:
         cell_three = pos 
@@ -35,12 +45,19 @@ def click_to_cell():
     elif pos[0] >= 200 and pos[0] < 400 and pos[1] >= 150 and pos[1] < 335:
         cell_five = pos
         print("Cell five")
-    elif pos[0] >= 400 and pos[0] < 600 and pos[1] >= 155 and pos[1] < 355:
+    elif pos[0] >= 400 and pos[0] < 595 and pos[1] >= 150 and pos[1] < 330:
         cell_six = pos 
         print("Cell six")
     elif pos[0] < 200 and pos[1] < 500:
         cell_seven = pos 
         print("Cell seven")
+    elif pos[0] >= 200 and pos[0] < 400 and pos[1] >= 335 and pos[1] < 500:
+        cell_eight = pos 
+        print("Cell eight")
+    elif pos[0] >= 400 and pos[0] < 600 and pos[1] >= 335 and pos[1] < 500:
+        cell_nine = pos 
+        print("Cell nine")
+    
 
 
 clock = pygame.time.Clock()
@@ -55,6 +72,7 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
             click_to_cell()
+            draw_XO()
 
     screen.fill((white))
     # draw vertical line on screen
