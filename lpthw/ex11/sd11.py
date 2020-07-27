96cb5be53c603eb9c2581c4a9ba27c34885ea39a
jan <janpoonthong628@gmail.com>
2020-07-27 08:11:24 +0700

    lpthw

---
+++ lpthw/ex11/sd11.py
@@ -0,0 +1,7 @@
+character = input("What character you like in marvel? \n\t* Thanos\n\t* Black Panther\n\t* Captain America \r\n:")
+language = input("\nWhat programming language you program in, name one? ")
+started_program = input("\nAt what age you started programming? ")
+
+print(f"\nYou like {character} in marvel.")
+print("You program in {}.".format(language))
+print(f"You started program at the age of {started_program}") 
