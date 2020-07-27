b9b7d8861dd6fda4c5cb51f6281c2bc17281e144
jan <janpoonthong628@gmail.com>
2020-07-27 09:17:34 +0700

    lpthw

---
+++ lpthw/ex13/sd_less.py
@@ -0,0 +1,5 @@
+from sys import argv
+script, name, age = argv
+
+print(f"Your name is {name}") 
+print(f"You're {age} year old")
---
+++ lpthw/ex13/sd_more.py
@@ -0,0 +1,8 @@
+from sys import argv
+script, name, age, language, gender = argv
+
+print(f"Your name is {name}")
+print(f"You're {age} year old")
+print(f"You program in {language}")
+print(f"You're {gender}")
+
