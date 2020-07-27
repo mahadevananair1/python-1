658b6340c42b03ad020d961a75b601051ceb8fdd
jan <janpoonthong628@gmail.com>
2020-07-27 15:51:20 +0700

    lpthw

---
+++ lpthw/ex18/ex18.py
@@ -0,0 +1,20 @@
+def print_two(*args):
+    arg1, arg2 = args
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+
+def print_none():
+    print("I got nothin'.")
+
+def main():
+   pass 
+
+print_two("Jan", "Stager")
+print_two_again("Jan", "Stager")
+print_one("First!")
+print_none()
