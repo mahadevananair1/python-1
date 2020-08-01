49c9f44dc3acb957bb3ddc06382dff466be05e1c
jan <janpoonthong628@gmail.com>
2020-08-01 21:44:44 +0700

    ex35

--- lpthw/ex35/ex35.py
+++ lpthw/ex35/ex35.py
@@ -18,3 +18,17 @@ def gold_room():
 def bear_room():
     print("There is a bear here")
     print("The bear has a bunch of honey")
+    print("The fat bear is in front of another door")
+    print("How are you going to move the bear?")
+    bear_moved = False
+
+    while True:
+        choice = input('> ')
+
+        if choice == "take honey":
+            dead("The bear looks at you then slaps your face off")
+        elif choice == "taunt bear" and not bear_moved:
+            print("The bear has moved from the door")
+            print("You can go through it now")
+            bear_moved = True
+
