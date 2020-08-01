2abfd2e451126a0dad10f95dbed9bc5aa967b8d2
jan <janpoonthong628@gmail.com>
2020-08-01 16:16:16 +0700

    ex33

---
+++ lpthw/ex33/ex33.py
@@ -0,0 +1,19 @@
+numbers = []
+final_num = 100
+
+def main():
+    i = 0
+    while i < final_num:
+        print(f"At the top i is {i}")
+        numbers.append(i)
+
+        i = i + 1
+
+        print("Numbers now: ", numbers)
+        print(f"At the bottom i is {i}")
+
+main()
+print("The numbers: ")
+
+for num in numbers:
+    print(num)
---
+++ lpthw/ex33/sd_ex33.py
@@ -0,0 +1,9 @@
+final_num = 5
+
+def main():
+    i = 0
+    while i < final_num:
+        print(f"The number is {i}")
+        i = i + 1
+
+main()
