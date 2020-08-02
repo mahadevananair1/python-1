111748ffd01b8f0c0c5651c2b093ba6ac41fe6f4
jan <janpoonthong628@gmail.com>
2020-08-02 09:41:12 +0700

    ex35.py

--- lpthw/ex35/ex35.py
+++ lpthw/ex35/ex35.py
@@ -4,10 +4,7 @@ def gold_room():
     print("This room is full of gold. How much do you take?")
 
     choice = input("> ")
-    if "0" in choice or "1" in choice:
-        how_much = int(choice)
-    else:
-        dead("Man, learn to type a number")
+    how_much = int(choice)
 
     if how_much < 50:
         print("Nice, you're not greedy, you win!")
