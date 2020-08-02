c47506ee4ef2a795b24ee8586204ac940be78b46
jan <janpoonthong628@gmail.com>
2020-08-02 12:34:07 +0700

    file

--- lpthw/ex36/ex36.py
+++ lpthw/ex36/ex36.py
@@ -1,16 +1,16 @@
-from sys import argv
+from sys import argv 
 
 script, file_name = argv
 
-in_file = open(file_name, 'r')
-indata = len(in_file.readline())
-print(indata)
+in_file = open(file_name)
+in_data = in_file.readlines()
 
-i = 0
-for i in range():
-    i += 1
-    print(f"{i}, {indata}")
+num = len(in_data)
 
+for line in in_data:
+    print(line)
 
+for count in range(1, num + 1):
+    print(count)
 
 in_file.close()
