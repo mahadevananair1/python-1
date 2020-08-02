e9f8be4e87abeab5e7e2a326967fa8d5e219ec35
jan <janpoonthong628@gmail.com>
2020-08-02 11:06:10 +0700

    ex36

---
+++ lpthw/ex36/ex36.py
@@ -0,0 +1,16 @@
+from sys import argv
+
+script, file_name = argv
+
+in_file = open(file_name, 'r')
+indata = len(in_file.readline())
+print(indata)
+
+i = 0
+for i in range():
+    i += 1
+    print(f"{i}, {indata}")
+
+
+
+in_file.close()
