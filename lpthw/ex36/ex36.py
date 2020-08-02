414f97803d8dbb027a38d841b7a41d7760907e92
jan <janpoonthong628@gmail.com>
2020-08-02 13:03:56 +0700

    ex36.py

--- lpthw/ex36/ex36.py
+++ lpthw/ex36/ex36.py
@@ -7,10 +7,12 @@ in_data = in_file.readlines()
 
 num = len(in_data)
 
-for line in in_data:
-    print(line)
+n = 0
+for elem in in_data:
+    n += 1
+    print(n, elem)
 
-for count in range(1, num + 1):
-    print(count)
+for i, l in enumerate(in_data):
+    print(i, l)
 
 in_file.close()
