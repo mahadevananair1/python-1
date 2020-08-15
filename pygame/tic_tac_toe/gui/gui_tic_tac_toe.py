2998d5c3982bb7b48b7726bf64c44f0d93ab874b
jan <janpoonthong628@gmail.com>
2020-08-15 13:16:40 +0700

    Create different way with rectangle

--- pygame/tic_tac_toe/gui/gui_tic_tac_toe.py
+++ pygame/tic_tac_toe/gui/gui_tic_tac_toe.py
@@ -1,11 +1,12 @@
 import pygame
 import os.path
+
 pygame.init()
 
-white = (255, 255, 255)
-line_color = (0, 0, 0)
+black = (0, 0, 0)
+line_color = (255, 255, 255)
 
-width, height = 600, 500
+width, height = 550, 550
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Tic Tac Toe")
 
@@ -21,79 +22,18 @@ width_resize, height_resize = 110, 110
 x_img = pygame.transform.scale(x_img, (width_resize, height_resize))
 o_img = pygame.transform.scale(o_img, (width_resize, height_resize))
 
-cell_one = False
-cell_two = False
-cell_three = False
-cell_four = False
-cell_five = False
-cell_six = False
-cell_seven = False
-cell_eight = False
-cell_nine = False
+# Draw rectangle
+first = pygame.draw.rect(screen, (line_color), (25, 25, 150, 150))
+second = pygame.draw.rect(screen, (line_color), (200, 25, 150, 150))
+third = pygame.draw.rect(screen, (line_color), (375, 25, 150, 150))
 
-def draw_xo():
-    global current_player
+fourth = pygame.draw.rect(screen, (line_color), (25, 200, 150, 150))
+fifth = pygame.draw.rect(screen, (line_color), (200, 200, 150, 150))
+sixth = pygame.draw.rect(screen, (line_color), (375, 200, 150, 150))
 
-    if cell_one is True:
-        screen.blit(x_img, (25,15))
-        flip_player()
-    if cell_two is True:
-        screen.blit(o_img, (250, 15))
-        flip_player()
-    if cell_three is True:
-        screen.blit(x_img, (450, 15))
-        flip_player()
-    if cell_four is True:
-        screen.blit(o_img, (25, 190))
-        flip_player()
-    if cell_five is True:
-        screen.blit(x_img, (250, 190))
-        flip_player()
-    if cell_six is True:
-        screen.blit(o_img, (450, 190))
-        flip_player()
-    if cell_seven is True:
-        screen.blit(x_img, (25, 370))
-        flip_player()
-    if cell_eight is True:
-        screen.blit(o_img, (250, 370))
-        flip_player()
-    if cell_nine is True:
-        screen.blit(x_img, (450, 370))
-        flip_player()
-
-def flip_player():
-    global current_player
-
-    if current_player == "X":
-        current_player = "O"
-    elif current_player == "O":
-        current_player = "X"
-
-def click_to_cell():
-    global cell_one, cell_two, cell_three, cell_four, cell_five, cell_six, cell_seven, cell_eight, cell_nine
-
-    if pos[0] < 200 and pos[1] < 150:
-        cell_one = True
-        print("Cell one")
-    elif pos[0] >= 200 and pos[0] < 400 and pos[1] < 150:
-        cell_two = True
-        print("Cell two")
-    elif pos[0] >= 400 and pos[0] < 600 and pos[1] < 150:
-        cell_three = True
-        print("Cell three")
-    elif pos[0] < 200 and pos[1] < 335:
-        print("Cell four")
-    elif pos[0] >= 200 and pos[0] < 400 and pos[1] >= 150 and pos[1] < 335:
-        print("Cell five")
-    elif pos[0] >= 400 and pos[0] < 595 and pos[1] >= 150 and pos[1] < 330:
-        print("Cell six")
-    elif pos[0] < 200 and pos[1] < 500:
-        print("Cell seven")
-    elif pos[0] >= 200 and pos[0] < 400 and pos[1] >= 335 and pos[1] < 500:
-        print("Cell eight")
-    elif pos[0] >= 400 and pos[0] < 600 and pos[1] >= 335 and pos[1] < 500:
-        print("Cell nine")
+seven = pygame.draw.rect(screen, (line_color), (25, 375, 150, 150))
+eight = pygame.draw.rect(screen, (line_color), (200, 375, 150, 150))
+nine = pygame.draw.rect(screen, (line_color), (375, 375, 150, 150))
 
 clock = pygame.time.Clock()
 fps = 60
@@ -101,23 +41,12 @@ fps = 60
 running = True
 while running:
     clock.tick(60)
-    screen.fill((white))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
-            click_to_cell()
-
-    # draw vertical line on screen
-    pygame.draw.line(screen, (line_color), (200, 0), (200, 500), 5)
-    pygame.draw.line(screen, (line_color), (400, 0), (400, 500), 5)
-
-    # draw horizontal line
-    pygame.draw.line(screen, (line_color), (0, 150), (600, 150), 5)
-    pygame.draw.line(screen, (line_color), (0, 335), (600, 335), 5)
-
-    draw_xo()
+            print(pos)
 
     pygame.display.update()
