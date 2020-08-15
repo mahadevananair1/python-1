7e44337af6d4e86d4268c69acbfbd0bc05ecb35d
jan <janpoonthong628@gmail.com>
2020-08-15 09:43:37 +0700

    Create ex39.py and python dict

---
+++ lpthw/ex39/ex39.py
@@ -0,0 +1,46 @@
+states = {
+    'Oregon': 'OR',
+    'Florida': 'FL',
+    'California': 'CA',
+    'New York': 'NY',
+    'Michigan': 'MI'
+}
+
+cities = {
+    'CA': 'San Francisco',
+    'MI': 'Detroit',
+    'FL': 'Jacksonville'
+}
+
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+print('-' * 10)
+print("NY State has: ", cities['NY'])
+print("OR State has: ", cities['OR'])
+
+print('-' * 10)
+print("Michigan has: ", cities[states['Michigan']])
+print("Florida has: ", cities[states['Florida']])
+
+print('-' * 10)
+for state, abbrev in list(states.items()):
+    print(f"{state} is abbreviated {abbrev}")
+
+print('-' * 10)
+for abbrev, city in list(states.items()):
+    print(f"{abbrev} has city {city}")
+
+print('-' * 10)
+for state, abbrev in list(states.items()):
+    print(f"{state} state is abbreviated {abbrev}")
+    print(f"and has city {cities[abbrev]}")
+
+print('-' * 10)
+state = states.get('Texas')
+print(state)
+if not state:
+    print("Sorry, no Texas")
+
+city = cities.get('TX', 'Does Not Exitst')
+print(f"The city for the state 'TX' is: {city}")
