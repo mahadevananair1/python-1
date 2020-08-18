f53310b4386d332fd5abea6f0d1cb13d5f2ae96f
jan <janpoonthong628@gmail.com>
2020-08-18 07:44:40 +0700

    For personal understanding

--- pygame/tic_tac_toe/gui/board.py
+++ pygame/tic_tac_toe/gui/board.py
@@ -1,18 +1,56 @@
-board = [[1, 1, 1], [0, 2, 2], [2, 2, 0]]
+board = [[1, 2, 0], [1, 1, 1], [1, 2, 0]]
 
 
-def check(num):
+# def check(num):
+#     for row in board:
+#         print(row)
+#         for tile in row:
+#             if tile == num:
+#                 continue
+#             else:
+#                 break
+#         else:
+#             print("X won")
+
+#     for column in range(3):
+#         for row in board:
+#             if row[column] == num:
+#                 continue
+#             else:
+#                 break
+#         else:
+#             print("X won")
+#
+# if check(1):
+#     won = True
+# if check(2):
+#     won = True
+
+# for column in range(3):
+#     print('\n')
+#     for row in board:
+#         print(row[column])
+
+# for column in range(3):
+#     for row in board:
+#         print(row)
+#         print(row[column])
+
+# column = 2
+# for row in board:
+#     print(row)
+#     print(row[column])
+
+board_first = [1, 2, 0]
+board_second = [1, 1, 1]
+board_third = [1, 2, 0]
+board = [board_first, board_second, board_third]
+for column in range(3):
     for row in board:
-        for tile in row:
-            if tile == num:
-                continue
-            else:
-                break
-        else:
-            print("X won")
+        print(row)
+        print(row[column])
 
-
-if check(1):
-    won = True
-if check(2):
-    won = True
+# column = 0
+# for row in board:
+#     print(row)
+#     print(row[column])
