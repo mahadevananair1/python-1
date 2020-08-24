06968a34a5756c12674192c239199dc85c67da4f
jan <janpoonthong628@gmail.com>
2020-08-24 09:44:42 +0700

    Mad libs

---
+++ pygame/mad_libs_generator/main.py
@@ -0,0 +1,6 @@
+adjective = input("Adjective: ")
+noun = input("Noun: ")
+plural_noun = input("Plural Noun: ")
+female_name = input("Female Name: ")
+
+print(f"""There are many "{adjective}" ways to choose a/an "{noun}" to read. First, you could ask for recommendations from your friends and "{plural_noun}".Just don't ask Aunt "{female_name}" she only reads "{adjective}" books.""")
