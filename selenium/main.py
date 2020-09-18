d9133566a0a561f28bf0493738b7f31a9d10df0b
Jan Poonthong <janpoonthong628@gmail.com>
2020-09-18 13:16:32 +0700

    Page navigating and click elements with link

--- selenium/main.py
+++ selenium/main.py
@@ -1,27 +1,33 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 PATH = '/home/jan/code/python/selenium/chromedriver'
 driver = webdriver.Chrome(PATH)
 
 driver.get('https://techwithtim.net')
-print(driver.title)
 
-search_bar = driver.find_element_by_name("s")
-search_bar.send_keys("Django")
-search_bar.send_keys(Keys.RETURN)
+link = driver.find_element_by_link_text("Game Development With Python")
+link.click()
 
 try:
-    main = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "main"))
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Snake Tutorial"))
     )
+    element.click()
 
-    articles = main.find_elements_by_tag_name("article")
-    for article in articles:
-        header = article.find_element_by_class_name("entry-summary")
-        print(header.text)
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "sow-button-19310003"))
+    )
+    element.click()
+
+    driver.back()
+    driver.back()
+    driver.back()
+    driver.forward()
+    driver.forward()
 finally:
+    time.sleep(2)
     driver.quit()
