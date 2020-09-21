b1e28984ff8e97ba0f04efe10ee170b5fb47ab5d
Jan Poonthong <janpoonthong628@gmail.com>
2020-09-21 12:06:54 +0700

    Create unit test with function and learning about assert

--- selenium/unit_test/main.py
+++ selenium/unit_test/main.py
@@ -8,6 +8,13 @@ class PythonOrgSearch(unittest.TestCase):
         self.driver = webdriver.Chrome("/home/jan/code/python/selenium/chromedriver")
         self.driver.get("http://www.python.org")
 
+    def test(self):
+        print("Test")
+        assert True
+
+    def not_test(self):
+        print("Not Test")
+
     def tearDown(self):
         self.driver.close()
 
