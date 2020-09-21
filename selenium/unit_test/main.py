93f622ec93261b394a7dbe9ca2f5ca0ea3011940
Jan Poonthong <janpoonthong628@gmail.com>
2020-09-21 16:24:34 +0700

    Create test_search_python

--- selenium/unit_test/main.py
+++ selenium/unit_test/main.py
@@ -8,9 +8,13 @@ class PythonOrgSearch(unittest.TestCase):
         self.driver = webdriver.Chrome("/home/jan/code/python/selenium/chromedriver")
         self.driver.get("http://www.python.org")
 
-    def test_title(self):
+    def test_search_python(self):
         main_page = page.MainPage(self.driver)
         assert main_page.is_title_matches()
+        main_page.search_text_element = "pydoc"
+        main_page.click_go_button()
+        search_result_page = page.SearchResultPage(self.driver)
+        assert search_result_page.is_results_found()
 
     def tearDown(self):
         self.driver.close()
