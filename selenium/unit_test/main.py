1e3e86a9a89c043b88b320f3321e62c71d0f5829
Jan Poonthong <janpoonthong628@gmail.com>
2020-09-21 14:59:10 +0700

    Change variable name mainPage -> main_page

--- selenium/unit_test/main.py
+++ selenium/unit_test/main.py
@@ -2,17 +2,19 @@ import unittest
 from selenium import webdriver
 import page
 
+
 class PythonOrgSearch(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome("/home/jan/code/python/selenium/chromedriver")
         self.driver.get("http://www.python.org")
 
     def test_title(self):
-        mainPage = page.MainPage(self.driver)
-        assert mainPage.is_title_matches()
+        main_page = page.MainPage(self.driver)
+        assert main_page.is_title_matches()
 
     def tearDown(self):
         self.driver.close()
+
 
 if __name__ == "__main__":
     unittest.main()
