78053a3946eac78d66ee604703f8538c3dc48d92
Jan Poonthong <janpoonthong628@gmail.com>
2020-09-21 12:01:36 +0700

    Create main.py for unit test and create setUp

--- selenium/unit_test/main.py
+++ selenium/unit_test/main.py
@@ -2,21 +2,11 @@ import unittest
 from selenium import webdriver
 import page
 
-class PythonOrgSearchTest(unittest.TestCase):
+class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
-        print("setup")
         self.driver = webdriver.Chrome("/home/jan/code/python/selenium/chromedriver")
         self.driver.get("http://www.python.org")
-
-    def test_example(self):
-        assert False
-
-    def test_example_2(self):
-        assert True
-
-    def not_a_test(self):
-        print("Not a test")
 
     def tearDown(self):
         self.driver.close()
