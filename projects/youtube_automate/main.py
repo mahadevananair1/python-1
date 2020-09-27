33fdfd086a1c560c1d70566bf86074c66099f2b3
Jan Poonthong <janpoonthong628@gmail.com>
2020-09-27 16:12:57 +0700

    Change to Migos

--- projects/youtube_automate/main.py
+++ projects/youtube_automate/main.py
@@ -4,7 +4,7 @@ driver = webdriver.Firefox(executable_path='./geckodriver')
 driver.get('https://youtube.com')
 search_box = driver.find_element_by_xpath('//input[@id="search"]')
 print(search_box)
-search_box.send_keys('devon crawford')
+search_box.send_keys('Migos')
 
 searchButton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')
 print(searchButton)
