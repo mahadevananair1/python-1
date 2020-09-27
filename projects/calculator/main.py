28733b181ac7787bbc9a41741844323c5ccf55b7
Jan Poonthong <janpoonthong628@gmail.com>
2020-09-27 10:44:49 +0700

    Add a button for clearing the answer and entry for displaying the answer

--- projects/calculator/main.py
+++ projects/calculator/main.py
@@ -1,10 +1,21 @@
-from tkinter import Tk
+from tkinter import Tk, Button, Entry
+
+
+def clear():
+    pass
+
+
+def buttons():
+    Button(screen, text="C", width="40", height="2", command=clear).pack()
 
 
 def main_screen():
+    global screen
     screen = Tk()
     screen.geometry("500x400")
     screen.title("Calculator")
+    Entry(screen, width="50", textvariable=answer).pack()
+    buttons()
 
     screen.mainloop()
 
