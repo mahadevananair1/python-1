769cc1abee80f474d175ab73cf745c84148defc3
Jan Poonthong <janpoonthong628@gmail.com>
2020-09-30 14:02:19 +0700

    AdVolKit script, work progress

--- selenium/advolkit/main.py
+++ selenium/advolkit/main.py
@@ -6,3 +6,5 @@ PATH = (os.path.join(project_directory, "../cookie/./chromedriver"))
 
 driver = webdriver.Chrome(PATH)
 driver.get("https://www.twitch.tv/advolkit")
+text_box = driver.find_element_by_xpath("""//*[@id="d6ba35aa81570f259da5f37122314b77"]/div/div[1]/div/div/div/div/div/section/div/div[4]/div[2]/div[1]/div/div[2]/div/div/textarea""")
+text_box.send_keys("!vox from script")
