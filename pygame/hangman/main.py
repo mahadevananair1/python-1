cf0209b4ef602c47b27987c85b36a601d3e9239e
Jan Poonthong <janpoonthong628@gmail.com>
2020-10-01 22:20:59 +0700

    Create hangman
    Add secret words
    Use random to pick any word from the list

---
+++ pygame/hangman/main.py
@@ -0,0 +1,9 @@
+import random
+
+secret_words = ["apple", "banana", "mango", "strawberry",
+                "orange", "grape", "pineapple", "apricot",
+                "lemon", "coconut", "watermelon", "cherry",
+                "papaya", "berry", "peach", "lychee", "muskmelon"]
+
+random_fruit = random.choice(secret_words)
+print(random_fruit)
