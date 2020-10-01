3b5414e8feb6285afd35c712dbe0783171aad7c1
Jan Poonthong <janpoonthong628@gmail.com>
2020-10-01 22:38:00 +0700

    Replace every word with _

--- pygame/hangman/main.py
+++ pygame/hangman/main.py
@@ -7,3 +7,9 @@ secret_words = ["apple", "banana", "mango", "strawberry",
 
 random_fruit = random.choice(secret_words)
 print(random_fruit)
+
+for letter in random_fruit:
+    print("_", end=" ")
+
+
+# if __name__ == "__main__":
