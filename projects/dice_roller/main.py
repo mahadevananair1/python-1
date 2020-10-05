196fe188d4e000c6cfafbcff33f96569caef6e5e
Jan Poonthong <janpoonthong628@gmail.com>
2020-10-05 23:18:04 +0700

    Fix the issue with merging code

--- projects/dice_roller/main.py
+++ projects/dice_roller/main.py
@@ -3,5 +3,8 @@ from random import randint
 repeat = True
 while repeat:
     print('You rolled', randint(1, 6))
-    x=input('Do you want to roll again? Y/N')
-    pass if x=='y' or 'yes' else break
+    x = input('Do you want to roll again? Y/N: ').lower()
+    if x == 'y' or x == 'yes':
+        repeat = True
+    else:
+        repeat = False
