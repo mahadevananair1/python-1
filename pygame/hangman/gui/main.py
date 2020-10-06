65a8ab1f9ca7c348e650c65ed34b17c8ac85fbfa
Jan Poonthong <janpoonthong628@gmail.com>
2020-10-06 16:30:54 +0700

    Add main menu, rendering text, and finishing touches
    Change GAP
    Understand the maths for letters
    Move while loop into a function

--- pygame/hangman/gui/main.py
+++ pygame/hangman/gui/main.py
@@ -12,10 +12,6 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 # Caption
 pygame.display.set_caption("Hangman Game")
 
-# FPS
-FPS = 60
-clock = pygame.time.Clock()
-
 # Font
 TITLE_FONT = pygame.font.Font("font/comic-shanns.otf", 70)
 LETTER_FONT = pygame.font.Font("font/comic-shanns.otf", 30)
@@ -36,15 +32,16 @@ for i in range(7):
 # Buttons
 # TODO(Jan): Understand the maths
 RADIUS = 20
-GAP = 10
+GAP = 15
 letters = []
-startx = round((WIDTH - (GAP + RADIUS * 2) * 13) / 2)
+startx = round((WIDTH - (RADIUS * 2 + GAP) * 13) / 2)  # answer = 42
 starty = 400
 A = 65
 for i in range(26):
-    x = startx + GAP * 2 + ((GAP + RADIUS * 2) * (i % 13))
+    x = startx + GAP * 2 + ((RADIUS * 2 + GAP) * (i % 13))
     y = starty + ((i // 13) * (GAP + RADIUS * 2))
     letters.append([x, y, chr(A + i), True])
+print(letters)
 
 # Colors
 BLACK = (0, 0, 0)
@@ -86,43 +83,75 @@ def display_message(message):
     text = WORD_FONT.render(message, True, BLACK)
     screen.blit(text, (WIDTH / 2 - text.get_width() / 2, HEIGHT / 2 - text.get_height() / 2))
     pygame.display.update()
-    pygame.time.delay(3000)
+    # pygame.time.delay(3000)
 
 
-run = True
-while run:
+def play_again():
+    global play, quit
+
+    play = LETTER_FONT.render("Play", True, BLACK)
+    screen.blit(play, (100, 400))
+    quit = LETTER_FONT.render("Quit", True, BLACK)
+    screen.blit(quit, (600, 400))
+    pygame.display.update()
+
+
+def main():
+    global hangman_status
+
     # FPS
-    clock.tick(FPS)
+    FPS = 60
+    clock = pygame.time.Clock()
+    run = True
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            m_x, m_y = pygame.mouse.get_pos()
-            for letter in letters:
-                x, y, ltr, visible = letter
-                if visible:
-                    # TODO(Jan): Understand the maths
-                    dis = math.sqrt((x - m_x) ** 2 + (y - m_y) ** 2)
-                    if dis < RADIUS:
-                        letter[3] = False
-                        guessed.append(ltr)
-                        if ltr not in word:
-                            hangman_status += 1
-    won = True
-    for letter in word:
-        if letter not in guessed:
-            won = False
+    while run:
+        # FPS
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                m_x, m_y = pygame.mouse.get_pos()
+                for letter in letters:
+                    x, y, ltr, visible = letter
+                    if visible:
+                        # TODO(Jan): Understand the maths
+                        dis = math.sqrt((x - m_x) ** 2 + (y - m_y) ** 2)
+                        if dis < RADIUS:
+                            letter[3] = False
+                            guessed.append(ltr)
+                            if ltr not in word:
+                                hangman_status += 1
+        draw()
+
+        won = True
+        for letter in word:
+            if letter not in guessed:
+                won = False
+                break
+
+        if won:
+            display_message("You won")
+            play_again()
             break
 
-    if won:
-        display_message("You won")
-        break
+        if hangman_status == 6:
+            display_message("You lose")
+            play_again()
+            break
 
-    if hangman_status == 6:
-        display_message("You lose")
-        break
+        pygame.display.update()
 
-    draw()
 
-    pygame.display.update()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            m_x, m_y = pygame.mouse.get_pos()
+            collision = math.sqrt((play.get_width() / 2 - m_x / 2) ** 2 + (play.get_height() / 2 - m_y / 2) ** 2)
+            if collision < 94:
+                print("true")
+            else:
+                print("false")
+
+    main()
